669f7e9224577970d69b8515aa159b443f1490e3
Windy <windywu812@gmail.com>
2020-06-17 19:23:47 +0700

    Initial Commit

---
+++ Programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmatically
+//
+//  Created by Windy on 17/06/20.
+//  Copyright © 2020 Windy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
